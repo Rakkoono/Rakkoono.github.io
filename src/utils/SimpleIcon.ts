c4bd6a431c16c9f34e60c662a8fd6badab8545c6
Rakkoono <samuel.birth@t-online.de>
2021-05-04 17:16:46 +0200

    Change icon references to point to simpleicons.org directly, added WIP /hexisles page

---
+++ src/utils/SimpleIcon.ts
@@ -0,0 +1,22 @@
+class SimpleIcon extends HTMLElement {
+    iconsURL: string = "https://simpleicons.org/icons/";
+    constructor() {
+        super();
+
+        this.style.cssText =
+           `display: block;
+            width: 50px;
+            height: 50px;
+
+            -webkit-mask-size: 100%; 
+            mask-size: 100%;`;
+        
+        if (this.hasAttribute("name"))
+        {
+            let url: string = "url(" + this.iconsURL + this.getAttribute("name") + ")";
+            this.style.cssText = "-webkit-mask-image: " + url + "; mask-image: " + url + ";";
+        }
+    }
+}
+
+window.customElements.define("simple-icon", SimpleIcon);
