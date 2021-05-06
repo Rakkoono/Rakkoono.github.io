0ce5d13322b122b8849f23649cb97e70ccc9bac8
Rakkoono <samuel.birth@t-online.de>
2021-05-06 17:21:24 +0200

    Add <site-media-data>-tag

---
+++ utils/MetaData.js
@@ -0,0 +1,17 @@
+"use strict";
+/*class MetaData extends HTMLElement {
+    iconsURL: string = "https://simpleicons.org/icons/";
+    constructor() {
+        super();
+
+        if ($(this).att.hasAttribute("title"))
+        {
+            
+        }
+            
+
+        $(this).append($())
+    }
+}
+
+window.customElements.define("meta-data", MetaData);*/ 
